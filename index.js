44fc17f74de83f89ae3401f6f91f6095e7d265e1
Shivani Rajpoot <83338429+sr1312@users.noreply.github.com>
2021-09-04 10:17:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+//Import express
+const { request, response } = require("express");
+const express = require("express");
+const app = express();
+const ytdl = require('ytdl-core');
+
+
+app.use(express.json());
+
+
+//Static file
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/image', express.static(__dirname + 'public/image'))
+
+
+
+app.set('view engine', 'ejs');
+
+app.get('/',(req, res) =>{
+    res.send("hi");
+});
+
+app.get('/home',(req, res) =>{
+    res.render('home.ejs')
+    });
+
+
+app.get('/download', (req, res) => {
+    var url = req.query.url;
+    res.header("Content-Disposition", 'attachment; filename="Video.mp4');
+    ytdl(url, {format: 'mp4'}).pipe(res);
+});
+
+
+
+
+app.listen(9000,(req, res) => {
+    console.log("listening on port 9000");
+});
+
+
+
